505b60110fc73fbf66cb2fea7bf10f695f9c32ab
Hugo HISMANS <hhismans@e1r8p13.42.fr>
2017-10-27 03:23:16 +0200

    add cube class

--- includes/Cube.class.hpp
+++ includes/Cube.class.hpp
@@ -6,19 +6,84 @@
 /*   By: hhismans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 01:53:59 by hhismans          #+#    #+#             */
-/*   Updated: 2017/10/25 04:53:28 by hhismans         ###   ########.fr       */
+/*   Updated: 2017/10/27 03:20:38 by hhismans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE_CLASS_HPP
 # define CUBE_CLASS_HPP
 
-#include <iostream>
+# include <iostream>
+# include <list>
+# include <vector>
+# include <algorithm> // std::find
 # include "opencv2/highgui/highgui.hpp"
 # include "opencv2/imgproc/imgproc.hpp"
 
-using namespace cv;
+using namespace cv; // SHAME
 using namespace std;
+
+enum Instruction
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	BACK,
+	FRONT,
+	RUP,
+	RDOWN,
+	RLEFT,
+	RRIGHT,
+	RBACK,
+	RFRONT
+}
+
+enum CornerPosition
+{
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEXEN,
+	EIGHT
+}
+
+enum Rotation
+{
+	CW, //clockwise
+	CCW // countterclockwise
+}
+enum CornerOrientation
+{
+	ALPHA,
+	BETA,
+	GAMMA
+}
+
+enum RidgePosition
+{
+	A_RIDGE,
+	B_RIDGE,
+	C_RIDGE,
+	D_RIDGE,
+	E_RIDGE,
+	F_RIDGE,
+	G_RIDGE,
+	I_RIDGE,
+	J_RIDGE,
+	K_RIDGE,
+	L_RIDGE
+}
+
+enum RidgeOrientation
+{
+	TRUE,
+	FALSE
+}
+
 class Cube
 {
 	public:
@@ -28,6 +93,21 @@ class Cube
 		~Cube( void );
 		void changeFace(int faceId, std::vector<int> face);
 		Cube &	operator=( Cube const & rhs);
+
+		//detection
+		void changeFace(int faceId, std::vector<int> face);
+
+		//algo
+		static void			move(Instruction instruction);
 	private:
+		/*
+		 * Cube decrit comme 8 coin et 12 arete.
+		 * Chaque arete 
+		 * */
+		Corner _corners[8];
+		Ridge  _ridges[12];
 };
+
+std::ostream &		operator<<(std::ostream & out, const Npuzzle &rhs);
+
 #endif
--- src/Cube.class.cpp
+++ src/Cube.class.cpp
@@ -6,7 +6,7 @@
 /*   By: hhismans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 01:53:40 by hhismans          #+#    #+#             */
-/*   Updated: 2017/10/25 04:53:54 by hhismans         ###   ########.fr       */
+/*   Updated: 2017/10/27 03:19:09 by hhismans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,3 +66,81 @@ Cube::~Cube( void )
 /*Cube &Cube::operator=( Cube const & rhs )
 {
 }*/
+
+// part 2
+#include "rubiks.class.hpp"
+
+
+Cube::~Cube(void) {}
+// cycle A -> B -> C -> D -> A [A,B,C,D]
+void Cube::cyclingRidge(std::vector<RidgePosition> & cycle, bool changeOrientation)
+{
+	Ridge r_tmp;
+	r_tmp = _ridges[cycle.back()];
+	for (int pos = 0; pos < cycle.size(); pos++)
+	{
+		_ridges[cycle[pos + 1]] = _ridges[cycle[pos]];
+		if (changeOrientation)
+			_ridges[cycle[pos + 1]].side = !_ridges[cycle[pos + 1]].side;
+	}
+		_ridges[cycle.front()] = r_tmp;
+		if (changeOrientation)
+			_ridges[cycle.front()] = ! _ridges[cycle.front()];
+}
+
+void Cube::up(Instruction instruction)
+{
+	cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+/*	Ridge r_tmp;
+	
+	r_tmp = _ridges[D_RIDGE];
+	for (RidgePosition pos = A_RIDGE; pos < D_RIDGE; pos++)
+		_ridges[pos + 1] = _ridges[pos];
+	_ridges[A_RIDGE] = r_tmp;*/ 
+}
+
+void Cube::move(Instruction instruction)
+{
+	std::vector<RidgePosition> cyclesRidge[12];
+	cyclesRidge[UP] = [ONE,TWO,THREE,FOUR];
+
+	switch (instruction)
+	{
+		case UP :	cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case DOWN : cyclingRidge([I_RIDGE,J_RIDGE,K_RIDGE,L_RIDGE], false);
+					cyclingCorner([FIVE,SIX,SEVEN,EIGHT], false);
+					break;
+		case LEFT : cyclingRidge([B_RIDGE,E_RIDGE,J_RIDGE,F_RIDGE], true);
+					cyclingCorner([TWO,,THREE,FOUR], true);
+					break;
+		case RIGHT: cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case BACK:  cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case FRONT: cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case RUP :	 cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case RDOWN : cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case RLEFT : cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case RRIGHT: cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case RBACK:  cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+		case RFRONT: cyclingRidge([A_RIDGE,B_RIDGE,C_RIDGE,D_RIDGE], false);
+					cyclingCorner([ONE,TWO,THREE,FOUR], false);
+					break;
+	}
+}
